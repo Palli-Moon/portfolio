067e29c4512d1de131a08e78e900b812245fad31
Palli Moon <pallimoon@gmail.com>
2024-11-25 19:58:30 +0000

    Add some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <Image src='/palli.jpg' alt='Next.js logo' width={180} height={38} priority />
-    </main>
+    <>
+      <header className='container mx-auto my-10'>
+        <div className='flex gap-8 flex-wrap items-center justify-center'>
+          <Image src='/palli.jpg' alt='Next.js logo' width={180} height={38} priority />
+          <div className='text-center'>
+            <h1 className='text-7xl shadow-sm font-bold'>Palli Moon</h1>
+            <h1 className='text-1xl text-gray-400'>(Gunnar Páll Gunnarsson)</h1>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
