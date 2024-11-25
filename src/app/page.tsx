a65ec559a06bb9df6d71fe43c27f2d95fd71f6a5
Palli Moon <pallimoon@gmail.com>
2024-11-25 21:29:37 +0000

    Messing around with some colours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-      <header className='container mx-auto my-10'>
+      <div className='container mx-auto my-10 p-5 bg-gradient-to-r dark:from-slate-800 from-slate-400 rounded-3xl shadow-2xl'>
         <div className='flex gap-8 flex-wrap items-center justify-center'>
-          <Image src='/palli.jpg' alt='Next.js logo' width={180} height={38} priority />
+          <Image className='rounded-full drop-shadow-2xl' src='/palli.jpg' alt='Next.js logo' width={180} height={38} priority />
           <div className='text-center'>
-            <h1 className='text-7xl shadow-sm font-bold'>Palli Moon</h1>
-            <h1 className='text-1xl text-gray-400'>(Gunnar Páll Gunnarsson)</h1>
+            <h1 className='text-7xl font-bold drop-shadow-2xl'>Palli Moon</h1>
+            <h2 className='text-1xl moon-gray drop-shadow-2xl'>(Gunnar Páll Gunnarsson)</h2>
           </div>
         </div>
-      </header>
+      </div>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,9 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    container: {
-      padding: '2rem',
-    },
     extend: {
       colors: {
+        gray: 'var(--gray)',
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
