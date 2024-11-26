24f069d4f68bd3d82d506a588d5295dd1ad239ee
Palli Moon <pallimoon@gmail.com>
2024-11-26 18:35:07 +0000

    Setup Hero with daisyui

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <div className='hero min-h-[70vh] bg-gradient-to-b'>
+    <div className='hero min-h-[70vh] bg-gradient-to-b from-[oklch(var(--n))]'>
       <div className='hero-content flex-col lg:flex-row'>
         <Image src='/palli.jpg' className='max-w-sm rounded-lg shadow-2xl' alt='image' width={200} height={200} />
         <div>
           <h1 className='text-5xl font-bold text-primary'>Palli Moon</h1>
-          <p className='py-6 text-muted-foreground text-secondary'>Big Brogrammer Palli</p>
+          <p className='text-muted-foreground text-secondary'>Software Developer, Audio Engineer, Musician, Lover...</p>
         </div>
       </div>
     </div>
