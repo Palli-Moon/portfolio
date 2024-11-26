aae02ed2b217370e5ce664338e1823342963b4a6
Palli Moon <pallimoon@gmail.com>
2024-11-26 17:55:53 +0000

    Add gradient

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <div className='hero min-h-screen'>
+    <div className='hero min-h-[70vh] bg-gradient-to-b from-slate-200'>
       <div className='hero-content flex-col lg:flex-row'>
         <Image src='/palli.jpg' className='max-w-sm rounded-lg shadow-2xl' alt='image' width={200} height={200} />
         <div>
