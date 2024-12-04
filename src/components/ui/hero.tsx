532044bd6f132ff78470621847b0d9c2c72f0fa9
Palli Moon <pallimoon@gmail.com>
2024-12-04 16:21:08 +0000

    Add navbar and tweaks

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <div className='hero min-h-[70vh] bg-gradient-to-b from-[oklch(var(--n))]'>
-      <div className='hero-content flex-col lg:flex-row'>
-        <Image src='/palli.jpg' className='max-w-sm rounded-lg shadow-2xl' alt='image' width={200} height={200} />
+    <div className='hero min-h-[40vh] bg-neutral-900'>
+      <div className='hero-content flex-col lg:flex-row gap-10'>
+        <Image src='/palli.jpg' className='max-w-sm rounded-lg shadow-2xl' alt='image' width={150} height={150} />
         <div>
           <h1 className='text-5xl font-bold text-primary'>Palli Moon</h1>
           <p className='text-muted-foreground text-secondary'>Software Developer, Audio Engineer, Musician, Lover...</p>
