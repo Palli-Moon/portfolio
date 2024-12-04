8213bbb14871c96e224ef65a7172314aa3c7e55d
Palli Moon <pallimoon@gmail.com>
2024-12-04 18:38:15 +0000

    Set up bio

--- src/components/ui/bio.tsx
+++ src/components/ui/bio.tsx
@@ -1,3 +1,17 @@
 export default function Bio() {
-  return <div>Bio</div>;
+  return (
+    <div className='md:container md:mx-auto p-8 text-justify'>
+      <p className='my-4'>
+        Gunnar Páll studied computer science at Reykjavík University and graduated with a Bachelor of Science degree in the spring of 2016. He has more than 9
+        years’ experience in software development. Mainly specializing in C# .NET Framework and JavaScript (Typescript) libraries and frameworks like Node,
+        React and Next. but has also worked with SQL, Business Central AL, and a plethora of other languages at his most recent job at Klappir Grænar Lausnir,
+        while at RU or on his own. He loves to learn new things and is constantly sharpening his skills in his free time.
+      </p>
+      <p className='my-4'>
+        Outside of software development his interests are mostly music related. He plays drums in an active band. He is also self-taught on many other
+        instruments and has a Bachelor of Arts (Honours) degree in audio engineering from SAE Institute in London. He also enjoys video games, traveling and
+        spending times with friends and family.
+      </p>
+    </div>
+  );
 }
--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
       <div className='hero-content flex-col lg:flex-row gap-10'>
         <Image src='/palli.jpg' className='max-w-sm rounded-lg shadow-2xl' alt='image' width={150} height={150} />
         <div>
-          <h1 className='text-5xl font-bold text-primary'>Palli Moon</h1>
+          <h1 className='text-5xl font-bold text-primary'>Gunnar Páll Gunnarsson</h1>
           <p className='text-muted-foreground text-secondary'>Software Developer, Audio Engineer, Musician, Lover...</p>
         </div>
       </div>
