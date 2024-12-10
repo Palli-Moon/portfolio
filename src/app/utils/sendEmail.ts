60519dca282f3508d2ecbc48e88d5ec7aa75c7d5
Palli Moon <pallimoon@gmail.com>
2024-12-10 16:52:56 +0000

    Finish contact section

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -5,12 +5,11 @@ import { Resend } from 'resend';
 export async function sendMail(replyTo: string, text: string) {
   const resend = new Resend(process.env.RESEND_API_KEY);
 
-  const res = await resend.emails.send({
+  return await resend.emails.send({
     from: 'pallimoon.com@pallimoon.com',
     replyTo,
-    to: 'pallimoon@gmail.com',
+    to: 'pallimoon+portfolio@gmail.com',
     subject: `Contact email from ${replyTo} on pallimoon.com`,
     text,
   });
-  console.log('🚀 ~ sendMail ~ res:', res);
 }
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,30 +4,62 @@ import Content from './content';
 import { FaEnvelope } from 'react-icons/fa6';
 import { sendMail } from '@/app/utils/sendEmail';
 import { FormEvent } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function Contact() {
+  const toastOptions = {
+    style: {
+      background: 'black',
+    },
+    success: {
+      style: {
+        color: 'green',
+      },
+    },
+    error: {
+      style: {
+        color: 'red',
+      },
+    },
+  };
+
   async function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-
     const data = new FormData(e.currentTarget);
-    sendMail(data.get('email') as string, data.get('text') as string);
+    const email = data.get('email') as string;
+    const text = (data.get('text') as string) || 'No text';
+
+    if (email) {
+      const res = await sendMail(email, text);
+      console.log('🚀 ~ onSubmit ~ res:', res);
+      if (res.error) {
+        toast.error('Not able to send request.');
+      } else {
+        toast.success('Email sent!');
+      }
+    } else {
+      toast.error('Please enter email.');
+    }
   }
 
   return (
-    <Content title='Contact'>
-      <p className='my-4'>Send me a message with your email and I will get int touch!</p>
-      <form onSubmit={onSubmit}>
-        <textarea className='textarea textarea-primary w-full rounded-lg min-h-32' placeholder='Message' name='text'></textarea>
-        <div className='my-3 flex gap-4'>
-          <label className='input input-bordered input-primary flex items-center gap-2 rounded-lg basis-4/5'>
-            <FaEnvelope className='text-primary' />
-            <input type='text' className='grow' placeholder='Email' name='email' />
-          </label>
-          <button className='btn btn-primary basis-1/5' type='submit'>
-            Send
-          </button>
-        </div>
-      </form>
-    </Content>
+    <>
+      <Content title='Contact'>
+        <p className='my-4'>Send me a message with your email and I will get int touch!</p>
+        <form onSubmit={onSubmit}>
+          <textarea className='textarea textarea-primary w-full rounded-lg min-h-32' placeholder='Message' name='text'></textarea>
+          <div className='my-3 flex gap-4'>
+            <label className='input input-bordered input-primary flex items-center gap-2 rounded-lg basis-4/5'>
+              <FaEnvelope className='text-primary' />
+              <input type='text' className='grow' placeholder='Email' name='email' />
+            </label>
+            <button className='btn btn-primary basis-1/5' type='submit'>
+              Send
+            </button>
+          </div>
+        </form>
+      </Content>
+      <Toaster toastOptions={toastOptions} />
+    </>
   );
 }
